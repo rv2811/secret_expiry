a09ab280e80dd7ea18c12a22d9a4b57db3f9c8bf
C_Suthar <Ravi.Suthar@expresspros.com>
2025-03-21 16:05:54 +0530

    added ps

--- terraform/secretexpiry.ps1
+++ terraform/secretexpiry.ps1
@@ -1,64 +1,82 @@
-#Connect-AzAccount -Identity
-$PSEmailServer = "smtp.gmail.com"
-$allsub = Get-AzSubscription
-$expiringSecrets = @()
-$output = @()
+﻿# Get today's date
+ 
+$today= get-date
+$expirationDate = (get-date).AddDays(3)
 
-$todaydate = Get-Date
+$subscription = Get-AzSubscription
+ 
+foreach ($sub in $subscription){
+$secretexpire = New-Object System.Collections.Generic.List[System.Object]
+$kv = Get-AzKeyvault
 
-foreach ($subscription in $allsub) {
-    
-    Set-AzContext -SubscriptionId a105df11-a901-4d46-84de-aba178a3da63
-    
-   $allkeyvault = Get-AzKeyVault
-    
-    foreach ($keyVault in $allkeyvault) {
-    $output = @( 
-        
-        $keyvaultsecrets = Get-AzKeyVaultSecret -VaultName $keyVault.VaultName | Select-Object VaultName, Name, Expires
+foreach ($keyvault in $kv)  { 
 
-        foreach ($secret in $keyvaultsecrets) {
-            
-            if ($secret.Expires) {
-                try {
-                    
-                    $secretexpirydate = [datetime]$secret.Expires
-                    $pendingdays = ($secretexpirydate - $todaydate).Days
-                    
-                      
-                    if ($pendingdays -le 7 -and $pendingdays -ge 0) {
-                        Write-Output "Secret '$($secret.Name)' in Vault '$($keyVault.VaultName)' is going to expire within 7 days, Expiration Date: $secretexpirydate." 
-                    } elseif ($pendingdays -gt 7) {
-                        Write-Output "Secret '$($secret.Name)' in Vault '$($keyVault.VaultName)' is not expiring within the next 7 days. Expiration Date: $secretexpirydate." 
-                    } else {
-                        Write-Output "Secret '$($secret.Name)' in Vault '$($keyVault.VaultName)' has already expired or is expired. Expired on $secretexpirydate." 
-                    } 
-                } catch {
-                    Write-Output "Invalid expiry date format for secret '$($secret.Name)' in Vault '$($keyVault.VaultName)'."
-                }
-            } else {
-                Write-Output "No Expiry Date: Secret '$($secret.Name)' in Vault '$($keyVault.VaultName)' does not have an expiration date set."
-            }
-        } )
+$Secret = Get-AzKeyVaultSecret -VaultName $keyvault.VaultName | where-Object { $_.Expires -ne $NULL} |
+    Select-Object Name, @{Name="ExpiryDate";Expression={$_.Expires}}
+ 
+
+ 
+foreach ($sec in $Secret) {
+   $secretexpirydate = [datetime]$sec.ExpiryDate
+   $pendingdays = ($secretexpirydate - $today).Days
+ 
+if ($pendingdays -le 7 -and $pendingdays -ge 0)  {
+    $secretexpire+= [pscustomobject]@{
+    Name = $sec.Name
+    ExpiryDate = $sec.ExpiryDate
+    PendingDays = $pendingdays
     }
+  }
 }
-   
+if ($secretexpire -ne $null ){
+ 
+$table = $secretexpire.GetEnumerator()  | ConvertTo-Html -Fragment -As Table
+ 
+# HTML Body
+ 
+$EmailBody = @"
+<p>Dear User,<br> Please Find below secerets expiring within 7 Days.</p>
+<br>
 
-   Write-Host ($output -join "`n")
-
-   $sendMailMessageSplat = @{
-    From = 'ravi <ravi8511002963@gmail.com>'
-    To = 'ankit <sutharravik5@gmail>'
-    Subject = 'Test mail'
-    Credential = ('ravi8511002963','rrzh vgcv qvrc fjgm')
-    UseSsl = $true
-    Port= 587
+<table style="width: 68%" style="border-collapse: collapse; border: 1px solid #008080;">
+<tr>
+<td bgcolor="#008080" style="color: #FFFFFF; font-size: large; height: 35px;"> 
+        Secrets expiring within 7 Days 
+</td>
+</tr>
+<tr style="border-bottom-style: solid; border-bottom-width: 1px; padding-bottom: 1px">
+<td style="width: 100%; height: 35px"> $table </td>
+</tr>
+</table>
+"@
+ 
+ 
+$emailSmtpServer = "smtp.gmail.com"
+$emailSmtpServerPort = "587"
+$emailSmtpUser = "aptestsmtp719"
+$emailSmtpPass = "ruxb gsxb gpwm hokm"
+# recipient 
+$emailFrom = "aptestsmtp719@gmail.com"
+$emailTo = "aptestsmtp719@gmail.com"
+# message
+$emailMessage = New-Object System.Net.Mail.MailMessage( $emailFrom , $emailTo )
+$emailMessage.Subject = "Secrets Expiring in 7 Days"
+$emailMessage.IsBodyHtml = $true
+$emailMessage.Body = $EmailBody
+#client 
+$SMTPClient = New-Object System.Net.Mail.SmtpClient( $emailSmtpServer , $emailSmtpServerPort )
+$SMTPClient.EnableSsl = $True
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential( $emailSmtpUser , $emailSmtpPass );
+$SMTPClient.Send( $emailMessage )
+ 
+}
 
 
 }
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Send-MailMessage @sendMailMessageSplat
 
-  
-#Send-MailMessage -SmtpServer '<ip of email relay>' -To '<employee email>' -From '<central email>' -Subject 'test' -Body 'this is a test' -Port 25
+}
+ 
+ # Get Secrets
+
+ 
